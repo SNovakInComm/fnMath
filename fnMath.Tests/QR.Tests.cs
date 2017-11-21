86cd2ddafe9e2a3285e80bd6764f1676d5b7632b
credential.helper <33043784+SNovakInComm@users.noreply.github.com>
2017-11-20 16:28:49 -0800

    added QR decomposition routine

---
+++ fnMath.Tests/QR.Tests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fnMath.Tests
+{
+    [TestFixture]
+    class QR
+    {
+
+        [Test]
+        public void TestQRDecommpositionGrahamSchmidt()
+        {
+            Matrix<double> A = new Matrix<double>(3, 3);
+            QR<double> B;
+            double err = 1e-8;
+
+            A[0][0] = 1.0;
+            A[1][0] = 3.0;
+            A[2][0] = 8.0;
+
+            A[0][1] = 2.0;
+            A[1][1] = 0.0;
+            A[2][1] = 4.0;
+
+            A[0][2] = 1.0;
+            A[1][2] = 4.0;
+            A[2][2] = 10.0;
+
+            B = new QR<double>(A);
+
+            Matrix<double> C = B.Q * B.R;
+
+            for (int i = 0; i < C.rows; i++)
+                for (int j = 0; j < C.columns; j++)
+                    Assert.That(Math.Abs(C[0][0] - A[0][0]) < err,
+                        "Too much error in QR Decomposition");
+        }
+
+    }
+}
---
+++ fnMath/QR.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fnMath
+{
+    public class QR<T>
+        where T : struct,
+        IComparable<T>,
+        IConvertible,
+        IEquatable<T>
+    {
+
+        // --------------------------------------------------
+        // ------------------------- Members
+        #region 
+
+        Matrix<T> _Q;
+        Matrix<T> _R;
+
+        #endregion
+
+        // --------------------------------------------------
+        // ------------------------- Constructors
+        #region Constructors
+
+        public QR(Matrix<T> A)
+        {
+            DecomposeGrahmSchmidt(A);
+        }
+
+        #endregion
+
+
+        // --------------------------------------------------
+        // ------------------------- Accessors
+        #region Accessors
+
+        public Matrix<T> Q { get { return _Q; } }
+
+        public Matrix<T> R { get { return _R; } }
+
+        #endregion
+
+        // --------------------------------------------------
+        // ------------------------- Public Methods
+        #region Public Methods
+
+        void DecomposeGrahmSchmidt(Matrix<T> A)
+        {
+            _Q = new Matrix<T>(A.rows, A.columns);
+            _R = new Matrix<T>(A.columns, A.columns);
+
+            dynamic sum;
+            dynamic norm;
+            int rows = _Q.rows;
+            int columns = _Q.columns;
+    
+            for(int i=0; i<rows; i++)   // Normalize
+            {
+                for(int j=0; j<columns; j++)    // Initialaize ever other value of Q to A
+                    _Q[i][j] = A[i][j];
+            }
+    
+            for(int j=0; j<columns; j++)
+            {
+                for(int k=j; k >0; k--)
+                {
+                    sum = 0;
+
+                    for (int i = 0; i < rows; i++)
+                    {
+                        dynamic q = _Q[i][k - 1];
+                        sum += q * A[i][j]; // project V onto U
+                    }
+
+                    _R[k - 1][j] = sum;
+                    for(int i=0; i<rows; i++)
+                        _Q[i][j] -= sum * _Q[i][k - 1]; // Subtract
+                }
+                norm = 0;
+                for (int i = 0; i < rows; i++)
+                {
+                    dynamic q = _Q[i][j];
+                    norm += q * q;
+                }
+                norm = Math.Sqrt(norm);
+
+                _R[j][j] = norm;
+        
+                for(int i=0; i<rows; i++)
+                    _Q[i][j] /= norm;
+            }
+        }
+
+        #endregion
+
+
+        // --------------------------------------------------
+        // ------------------------- Private Methods
+        #region 
+
+
+
+        #endregion
+
+        // --------------------------------------------------
+        // ------------------------- Utility Functions
+        #region Utility Functions
+
+        public void Print()
+        {
+            System.Console.Write("--------Q\n");
+            for (int i = 0; i < Q.rows; i++)
+            {
+                for (int j = 0; j < Q.columns; j++)
+                    System.Console.Write(_Q[i][j] + "\t");
+                System.Console.Write("\n");
+            }
+            System.Console.Write("--------R\n");
+            for (int i = 0; i < R.rows; i++)
+            {
+                for (int j = 0; j < R.columns; j++)
+                    System.Console.Write(R[i][j] + "\t");
+                System.Console.Write("\n");
+
+            }
+            System.Console.Write("--------Q * R\n");
+            (Q * R).Print();
+        }
+
+        #endregion
+
+    }
+}
