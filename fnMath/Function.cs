3eefc565ef5d29385993e077321f798e7fb2846a
credential.helper <33043784+SNovakInComm@users.noreply.github.com>
2017-11-21 09:09:30 -0800

    Added function interface

---
+++ fnMath/Function.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fnMath
+{
+    interface Function<T>
+    {
+        T Evaluate(T x);
+
+        T Evaluate(T[] X);
+    }
+}
